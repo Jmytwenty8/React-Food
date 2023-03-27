2a0c777d6496024c013f4f31ab1cffd23e31a510
Jmytwenty8 <jmy2898@gmail.com>
2023-03-27 18:48:38 +0530

    Cors Fix Attempt on Netlify

---
+++ functions/proxy.js
@@ -0,0 +1,16 @@
+const fetch = require('node-fetch');
+
+exports.handler = async (event, context) => {
+  const { url } = event.queryStringParameters;
+
+  try {
+    const response = await fetch(url);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(await response.json()),
+    };
+  } catch (err) {
+    return { statusCode: 500, body: err.toString() };
+  }
+};
